f44dd5bf02a235b267e2dcce8bc0e2f88b74f47e
blakeNaccarato <blake.naccarato@gmail.com>
2022-08-22 23:25:09 +0000

    Get basic network working

--- SIM_thermal/RUN_test/input.py
+++ SIM_thermal/RUN_test/input.py
@@ -1,8 +1,23 @@
-exec(open("RUN_test/my_results.dr").read())
-mySimObject.model.capacitor.setCapacitance(1)
-print("Capacitance: {}".format(mySimObject.model.capacitor.getCapacitance()))
-mySimObject.model.potential.setSourcePotential(1)
-print("Source potential: {}".format(mySimObject.model.potential.getSourcePotential()))
-mySimObject.model.conductor.setDefaultConductivity(1)
-print("Conductivity: {}".format(mySimObject.model.potential.getEffectiveConductivity()))
-trick.stop(1.0)
+trick.sim_services.exec_set_trap_sigfpe(1)
+simControlPanel = trick.SimControlPanel()
+trick.add_external_application(simControlPanel)
+trickView = trick.TrickView()
+trick.add_external_application(trickView)
+# trickView.set_auto_open_file('TV_systems.tv')
+trick.real_time_enable()
+trick.sim_services.exec_set_terminate_time(10)
+trick.exec_set_software_frame(0.001)
+trick.TMM_reduced_checkpoint(False)
+trick_mm.mm.set_expanded_arrays(True)
+trick_sys.sched.set_enable_freeze(True)
+trick_sys.sched.set_freeze_command(True)
+
+# exec(open("RUN_test/my_results.dr").read())
+
+# # Input
+# mySimObject.model.netInput.capacitor.mCapacitance = 10
+# mySimObject.model.netInput.thermal_potential.mSourcePotential = 150
+# mySimObject.model.netInput.thermal_potential2.mSourcePotential = 50
+
+# # Config
+# mySimObject.model.netConfig.conductor.mDefaultConductivity = 10
