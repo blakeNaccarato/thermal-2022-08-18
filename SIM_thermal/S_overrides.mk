ccb9d52348ff39cb198aed3631fcc98b887cb44d
blakeNaccarato <blake.naccarato@gmail.com>
2022-08-23 19:45:30 +0000

    Generalize the simulation

--- SIM_thermal/S_overrides.mk
+++ SIM_thermal/S_overrides.mk
@@ -1,2 +1,7 @@
-include S_gunnsdraw_buildtime.mk
+GUNNSDRAWINGS := \
+ network/Network.xml
+GUNNSDRAW_HEADERS := $(subst .xml,.hh,$(GUNNSDRAWINGS))
+S_define: $(GUNNSDRAW_HEADERS)
+$(GUNNSDRAW_HEADERS) : %.hh : %.xml
+	@ echo $(shell python $(GUNNS_HOME)/draw/netexport.py $<)
 export TRICK_GTE_EXT = GUNNS_HOME
