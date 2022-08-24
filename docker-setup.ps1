bb97b8d9b8f8ad41eefafcb2d19c703c785f6366
blakeNaccarato <blake.naccarato@gmail.com>
2022-08-24 12:48:40 -0700

    Correct the cshrc reference

--- docker-setup.ps1
+++
@@ -1,8 +0,0 @@
-$Name = Get-Location | Split-Path -Leaf
-if ((docker volume inspect $Name) -eq '[]') {
-    docker volume create $Name
-}
-else {
-    docker volume rm $Name
-}
-docker-compose up --build -d
