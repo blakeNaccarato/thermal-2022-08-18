b37f3a0779c129f2244e7b547c36757e2f877694
blakeNaccarato <blake.naccarato@gmail.com>
2022-08-23 17:05:15 -0700

    Switch back to docker run workflow

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,10 @@
-$Name = "gunns-sims_volume"
+
+$Name = "gunns-sims"
+
 docker volume create --name $Name
+docker build . -t $Name
+
 $Volume = "\\wsl$\docker-desktop-data\data\docker\volumes\$Name\_data"
 if ( !(Test-Path $Volume/.git) ) { git clone . $Volume }
-docker-compose up -d
+
+docker run -id -v $Name:/root/$Name --name $Name $Name
